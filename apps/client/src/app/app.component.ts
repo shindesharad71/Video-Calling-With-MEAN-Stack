24425649e774723e40a214b1283e513a8f4c0598
Sharad Shinde <sharad.shinde@coditas.com>
2020-11-21 11:45:33 +0530

    Added: Live Video and Audio

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'sharad-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('videoRef') videoRef: ElementRef;
 
+  ngAfterViewInit(): void {
+    this.startUserMedia();
+  }
+
+  startUserMedia(): void {
+    const n = <any>navigator;
+    n.getUserMedia =
+      n.getUserMedia ||
+      n.webkitGetUserMedia ||
+      n.mozGetUserMedia ||
+      n.msGetUserMedia;
+
+    n.getUserMedia(
+      { audio: true, video: true },
+      (stream) => {
+        this.videoRef.nativeElement.srcObject = stream;
+        this.videoRef.nativeElement.play();
+      },
+      (err) => console.error(err)
+    );
+  }
 }
